bfc3839e325af3faa30101dbbb9e435be98eeae0
Erik <eitu@mail.ru>
2026-03-02 13:09:07 +0300

    .

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,14 +33,14 @@ class AuthController extends Controller {
         public function login(Request $request) {
             $credentials = $request->only('email', 'password');
             if (!$token = Auth::attempt($credentials)) {
-                return response()->json(['error' => 'Invalid credentials'], 401);
+                return response()->json(['error' => 'Неверные данные'], 401);
             }
             return response()->json(['token' => $token]);
         }
         // Logout User (Invalidate Token)
         public function logout() {
             Auth::logout();
-            return response()->json(['message' => 'Successfully logged out']);
+            return response()->json(['message' => 'Выход выполнен успешно']);
         }
         // Get Authenticated User
         public function me() {
